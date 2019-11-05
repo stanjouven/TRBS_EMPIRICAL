824b4af8b6abbf5393145983a95ce5b9c8bedf29
stanislas jouven <stanislasjouven@gmail.com>
2019-11-05 21:30:21 +0100

    change the way to add info to the dataframe

--- source_loc.py
+++ source_loc.py
@@ -21,6 +21,7 @@ def trbs_empirical(graph, obs_time_filt, distribution):
 
     path_lengths = preprocess(obs_filt, graph, distribution, nb_diffusions)
     print('final path ', path_lengths)
+    path_lengths = compute_mean_shortest_path(path_lengths)
 
     ### Run the estimation
     s_est, likelihoods = se.source_estimate(graph, obs_time_filt, path_lengths)
@@ -37,26 +38,24 @@ PARAMETERS:
 Return dictionnary: node -> time to go from that node to the given observer
 '''
 def preprocess(observer, graph, distr, nb_diffusions):
-    path_lengths = initialize_dataframe(observer)
+    path_lengths = pd.DataFrame()
     i = 0
     for diff in range(nb_diffusions):
+        path_lengths_temp = pd.DataFrame()
         ### Initialization of the edge delay
         edges = graph.edges()
         for (u, v) in edges:
             graph[u][v]['weight'] = graph[u][v]['weight'] + abs(distr.rvs())
         for o in observer:
             ### Computation of the shortest paths from every observer to all other nodes
-            path_lengths[str(o)] += pd.Series(nx.single_source_dijkstra_path_length(graph, o))
+            path_lengths_temp[str(o)] = pd.Series(nx.single_source_dijkstra_path_length(graph, o))
         if i == 0:
             print('path 1 ',path_lengths.to_dict())
         i = i + 1
-    path_lengths = path_lengths / len(observer)
-    return path_lengths.to_dict()
-
-
-
-def initialize_dataframe(observer):
-    path_lengths = pd.DataFrame()
-    for o in observer:
-        path_lengths[str(o)] = 0
+        path_lengths = path_lengths.append(path_lengths_temp)
     return path_lengths
+
+def compute_mean_shortest_path(path_lengths):
+    path_lengths.reset_index(inplace = True)
+    path_lengths = path_lengths.rename({'index': 'node'}, axis = 1).set_index('node')
+    return path_lengths.groupby(['node']).mean().to_dict()
