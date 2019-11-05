91c3470e5e62127bf74241d0bbe64bea54b7f1d4
stanislas jouven <stanislasjouven@gmail.com>
2019-11-05 17:28:33 +0100

    do not use str for nodes o

--- source_loc.py
+++ source_loc.py
@@ -44,7 +44,7 @@ def preprocess(observer, graph, distr, nb_diffusions):
             graph[u][v]['weight'] = graph[u][v]['weight'] + abs(distr.rvs())
         for o in observer:
             ### Computation of the shortest paths from every observer to all other nodes
-            path_lengths[str(o)] += pd.Series(nx.single_source_dijkstra_path_length(graph, o))
+            path_lengths[o] += pd.Series(nx.single_source_dijkstra_path_length(graph, o))
     path_lengths = path_lengths / len(observer)
     return path_lengths.to_dict()
 
