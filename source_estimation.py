dce582a972c9f4085d45041fa1736ae1d8115a74
stanislas jouven <stanislasjouven@gmail.com>
2019-11-05 17:36:45 +0100

    initialiaze the dictionnary with sedfault

--- source_estimation.py
+++ source_estimation.py
@@ -4,9 +4,11 @@ import collections
 
 
 def source_estimate(graph, obs_time, path_lengths):
-    T = collections.defaultdict(list)
+    #T = collections.defaultdict(list)
+    T = {}
     var_T = {}
     for node in list(graph.nodes()):
+        T.setdefault(node, [])
         for obs in np.array(list(obs_time.keys())):
             T[node].append(obs_time[obs] - path_lengths[obs][node])
         var_T[node] = np.var(T[node])
