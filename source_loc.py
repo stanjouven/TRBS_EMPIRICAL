1e77b52050a1d5220f9e38f85e02e8432a6830f8
stanislas jouven <stanislasjouven@gmail.com>
2019-11-09 11:17:41 +0100

    deleted prints

--- source_loc.py
+++ source_loc.py
@@ -22,8 +22,6 @@ def trbs_empirical(graph, obs_time_filt, distribution):
 
     path_lengths = preprocess(obs_filt, graph, distribution, nb_diffusions)
     path_lengths = compute_mean_shortest_path(path_lengths)
-    print('mean -------------------')
-    print(path_lengths)
 
     ### Run the estimation
     s_est, likelihoods = se.source_estimate(graph, obs_time_filt, path_lengths)
@@ -52,10 +50,7 @@ def preprocess(observer, graph, distr, nb_diffusions):
         for o in observer:
             ### Computation of the shortest paths from every observer to all other nodes
             path_lengths_temp[str(o)] = pd.Series(nx.single_source_dijkstra_path_length(graph, o))
-            print('...............')
-            print(nx.single_source_dijkstra_path_length(graph, o))
         path_lengths = path_lengths.append(path_lengths_temp)
-        print('____________________________________________________')
     return path_lengths
 
 
